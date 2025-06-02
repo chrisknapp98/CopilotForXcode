fc1b0f9299ad8045061c464ae22a36925c20cc60
Christopher Knapp <chris.knapp@live.de>
2025-06-02 17:23:22 +0200

    add directory picker for convenient choosing

--- Core/Sources/HostApp/Benchmark/Presentation/View/OutputConfigurationView.swift
+++ Core/Sources/HostApp/Benchmark/Presentation/View/OutputConfigurationView.swift
@@ -5,33 +5,56 @@ struct OutputConfigurationView: View {
     @StateObject var viewModel: OutputConfigurationViewModel
     @Environment(\.dismiss) private var dismiss
 
-    private let labelWidth: CGFloat = 100
+    private let labelWidth: CGFloat = 120
     
     init(module: BenchmarkModuleType) {
         self._viewModel = StateObject(wrappedValue: module.provide())
     }
     
     var body: some View {
-        HStack {
-            Text("Output Directory:")
-                .frame(width: labelWidth, alignment: .leading)
-            TextField("Output Directory", text: $currentOutputDirectory, prompt: Text("Directory"))
-                .textFieldStyle(PlainTextFieldStyle())
-                
-        }
-        .padding()
-        HStack {
-            Button("Cancel") {
-                dismiss()
+        VStack {
+            HStack(alignment: .top) {
+                Text("Output Directory:")
+                    .frame(width: labelWidth, alignment: .leading)
+                VStack {
+                    TextField("Output Directory", text: $currentOutputDirectory, prompt: Text("Directory"))
+                        .textFieldStyle(PlainTextFieldStyle())
+                    Button {
+                        selectDirectory()
+                    } label: {
+                        Label("Select...", systemImage: "folder")
+                    }
+                    .padding()
+                }
             }
-            Button("Save") {
-                viewModel.saveOutputDirectory(currentOutputDirectory)
-                dismiss()
+            .padding()
+            HStack {
+                Button("Cancel") {
+                    dismiss()
+                }
+                Button("Save") {
+                    viewModel.saveOutputDirectory(currentOutputDirectory)
+                    dismiss()
+                }
             }
+            .padding([.horizontal, .bottom])
         }
-        .padding([.horizontal, .bottom])
         .onAppear {
             currentOutputDirectory = viewModel.outputDirectory
+        }
+    }
+    
+    private func selectDirectory() {
+        let panel = NSOpenPanel()
+        panel.canChooseFiles = false
+        panel.canChooseDirectories = true
+        panel.allowsMultipleSelection = false
+        panel.directoryURL = URL(fileURLWithPath: currentOutputDirectory)
+
+        if panel.runModal() == .OK {
+            if let url = panel.url {
+                currentOutputDirectory = url.path
+            }
         }
     }
 }
